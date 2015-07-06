25c9994b395d0e38758ff99cc42169dd4a8d8b2c
= <=>
2015-07-05 19:52:31 -0700

    Better function names

--- image.py
+++ image.py
@@ -7,7 +7,7 @@ from IPython.display import clear_output, Image, display
 
 
 # Guess this saves an array as an image file, then displays it?
-def showarray(a, fmt='jpeg'):
+def show(a, fmt='jpeg'):
 	a = np.uint8(np.clip(a, 0, 255))
 	f = StringIO()
 	PIL.Image.fromarray(a).save(f, fmt)
@@ -15,10 +15,10 @@ def showarray(a, fmt='jpeg'):
 
 
 # Load an image into an array by file path
-def load_image(path):
+def load(path):
 	return np.float32(PIL.Image.open(path))
 
 
 # Save image from array
-def save_image(a, path, fmt='jpeg'):
+def save(a, path, fmt='jpeg'):
 	PIL.Image.fromarray(np.uint8(a)).save(path, fmt)
